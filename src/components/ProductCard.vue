b3d8efcb7438e30667674a44cc367950229bcc0c
shepherd.fan <pucca1988z@gmail.com>
2021-01-12 13:53:53 +0800

    done tutorial

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,14 +3,15 @@
     <div class="col-span-2 md:col-span-1">photo</div>
     <div class=" col-span-3 p-2 space-y-1">
       <router-link 
-        :to="{name:'Product', params:{id:23}}"
+        :to="{name:'Product', params:{productId:product._id}}"
         class=" text-blue-400 underline"
       > 
-        Product Title 
+        {{product.title}}
       </router-link>
-      <div> $23 </div>
+      <div> ${{product.price}} </div>
       <div> Lorem ipsum dolor sit amet consectetur. </div>
       <button 
+        @click="addToCart"
         class="border border-white rounded-md bg-blue-400 text-white hover:bg-blue-900 duration-300  px-2 py-1">
         Add to cart
       </button>
@@ -20,7 +21,15 @@
 
 <script>
 export default {
-
+  props:['product'],
+  methods:{
+    addToCart(){
+      this.$store.dispatch('addProductToCart', {
+        product: this.product,
+        qty:1
+      })
+    },
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -10,24 +12,59 @@ export default new Vuex.Store({
       {itemName:'cde', itemPrice:2, amount:5},
       {itemName:'xyz', itemPrice:3, amount:3}
     ],
-    products:[]
+    products:[],
+    product:null,
+    cart:[],
+    cartTotalPrice:0
   },
   getters:{
     testCartDataTotal(state){
       return state.testCartData.reduce((acc, cur) => {
         return acc + cur.itemPrice * cur.amount
       },0)
+    },
+    getCartTotalPrice(state){
+      let total = 0 
+      state.cart.forEach(item => total += item.product.price * item.qty)
+      return total
     }
   },
   mutations: {
     REMOVE_CART_ITEM(state, data){
-      let ind = state.testCartData.find( cartData => cartData == data )
-      state.testCartData.splice(ind, 1);
+      let ind = state.cart.find( cartData => cartData.product == data.product )
+      state.cart.splice(ind, 1);
+    },
+    SET_PRODUCTS(state, products){
+      state.products = products;
+    },
+    SET_PRODUCT(state, product){
+      state.product = product
+    },
+    ADD_TO_CART(state,{product, qty}){
+      let productInCart = state.cart.find( item => item.product._id == product._id)
+      console.log(qty)
+      if(productInCart) productInCart.qty = parseInt(qty) + parseInt(productInCart.qty)
+      else state.cart.push({product, qty})
     }
   },
   actions: {
     removeMiniCartItem({commit}, data){
       commit('REMOVE_CART_ITEM', data)
+    },
+    getProducts({commit}){
+      axios.get('http://localhost:4000/product')
+      .then( res => {
+        commit('SET_PRODUCTS', res.data.products)
+      })
+    },
+    getProduct({commit}, productId){
+      axios.get(`http://localhost:4000/product/${productId}`)
+      .then( res => {
+        commit('SET_PRODUCT', res.data.product)
+      })
+    }, 
+    addProductToCart({commit}, {product, qty}){
+      commit('ADD_TO_CART', {product, qty})
     }
   },
   modules: {
